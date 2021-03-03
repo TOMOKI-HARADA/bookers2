80cdee71542421868d388ccb3534f6cde5a67de9
TOMOKI-HARADA <ec2-user@ip-172-31-69-52.ec2.internal>
2021-03-03 13:17:41 +0000

    編集5-10

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,14 @@
 class ApplicationController < ActionController::Base
- 
-  protect_from_forgery with: :exception
-
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  protected
+ before_action :configure_permitted_parameters, if: :devise_controller?
+def after_sign_in_path_for(resource)
+  user_path(resource.id)
+end 
+  
+     protected
 
   def configure_permitted_parameters
-    added_attrs = [:user_name, :email, :password, :password_confirmation, :remember_me]
-    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
-    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
-  end
+      devise_parameter_sanitizer.permit(:sign_up,keys:[:name])
+    
+  devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+end
 end
