0f417cc8779fe10ed4705e71bf664ec1cd96952a
TOMOKI-HARADA <ec2-user@ip-172-31-69-52.ec2.internal>
2021-03-16 15:26:47 +0000

    レイアウト

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,17 +1,19 @@
 class BooksController < ApplicationController
 
-  def new
+ def show
+    @newbook = Book.new
+    @book = Book.find(params[:id])
+    @user = @book.user
+  end
+
+  def index
+    @user = current_user
     @book = Book.new
+    @books = Book.all
+  end
 
-end
-
-def index
-  @books = Book.page(params[:page]).reverse_order
-
-end
-
-def create
-  @book = Book.new(book_params)
+  def create
+     @book = Book.new(book_params)
   @book.user_id = current_user.id
   if  @book.save
     flash[:notice]= "You have created book successfully."
@@ -24,40 +26,40 @@ def create
   # @book.save
 end
 
-def show
+  def edit
+    @book = Book.find(params[:id])
+    if @book.user == current_user
+        render "edit"
+    else
+        redirect_to books_path
+    end
+  end
 
-  @book = Book.find(params[:id])
-  @user = @book.user
-end
+  def update
+    @book = Book.find(params[:id])
+    @book.user_id = current_user.id
+    if @book.update(book_params)
+      flash[:notice]="Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
 
-def edit
-  @book = Book.find_by(id: params[:id])
+  def destroy
+    @book = Book.find(params[:id])
+    if @book.destroy
+      flash[:notice]="Book was successfully destroyed."
+      redirect_to books_path
+    end
+  end
+  def search
+@searched_books = Book.search(params[:search])
+  @searched_books = @searched_books.page(params[:page])
+end 
 
-end
-
-def update
-  book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
-end
-
-def destroy
-  @book = Book.find(params[:id])
-  @book.destroy
-  redirect_to books_path
-
-end
-
-private
-def correct_user
-  @book = Book.find(params[:id])
-  redirect_to(boooks_path) unless current_user?(@book.user)
-end
-
-def current_user?(user)
-  user == current_user
-end
-def book_params
-  params.require(:book).permit(:title, :body, :user_id)
-end
+  private
+    def book_params
+      params.require(:book).permit(:title, :body)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   root to: "homes#top"
-  devise_for :users
- get "top" => "homes#top"
-  get "about" => "homes#about"
+  devise_for :users, controllers: {
+    sessions: 'devise/sessions',
+    registrations: 'devise/registrations'
+  }
+
+  root "homes#top"
+
+  get "/home/about" => "homes#about"
   resources :users
   resources :books
-  get "books/:id/edit" => "books#edit"
-  patch "books/:id/" => "books#update"
-  post "boooks/:id/" => "books#create" 
-   delete "books/:id/" => "books#destroy"
- post "users/:id" => "books#new"
  
   end
