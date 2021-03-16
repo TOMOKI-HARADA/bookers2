399e77769a5a52f2602f5ec5985fc6d7d1a0b279
TOMOKI-HARADA <ec2-user@ip-172-31-69-52.ec2.internal>
2021-03-16 16:20:03 +0000

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
   root to: "homes#top"
-  devise_for :users, controllers: {
-    sessions: 'devise/sessions',
-    registrations: 'devise/registrations'
-  }
+  devise_for :users
 
-  root "homes#top"
+  
 
   get "/home/about" => "homes#about"
   resources :users
